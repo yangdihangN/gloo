24c941ad5d840d7cd071b6ae9b22abb29efadc47
Scott Weiss <sdw35@cornell.edu>
2019-01-02 18:21:46 -0500

    implement status syncer
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- projects/ingress/pkg/status/status_syncer.go
+++ projects/ingress/pkg/status/status_syncer.go
@@ -2,33 +2,32 @@ package status
 
 import (
 	"context"
+	"github.com/gogo/protobuf/proto"
+	"github.com/pkg/errors"
+	"github.com/solo-io/gloo/projects/ingress/pkg/api/ingress"
+	"github.com/solo-io/gloo/projects/ingress/pkg/api/service"
+	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
+	kubev1 "k8s.io/api/core/v1"
+	"net"
+	"sort"
 
-	"github.com/solo-io/gloo/projects/gateway/pkg/todo"
-	gloov1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/gloo/projects/ingress/pkg/api/v1"
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
-	"github.com/solo-io/solo-kit/pkg/api/v1/reporter"
 	"github.com/solo-io/solo-kit/pkg/utils/contextutils"
 )
 
 type statusSyncer struct {
-	writeNamespace string
-	reporter       reporter.Reporter
-	writeErrs      chan error
-	ingressClient  v1.IngressClient
+	ingressClient v1.IngressClient
 }
 
-func NewSyncer(writeNamespace string, proxyClient gloov1.ProxyClient, ingressClient v1.IngressClient, reporter reporter.Reporter, writeErrs chan error) v1.ApiSyncer {
+func NewSyncer(ingressClient v1.IngressClient) v1.StatusSyncer {
 	return &statusSyncer{
-		writeNamespace: writeNamespace,
-		reporter:       reporter,
-		writeErrs:      writeErrs,
-		ingressClient:  ingressClient,
+		ingressClient: ingressClient,
 	}
 }
 
 // TODO (ilackarms): make sure that sync happens if proxies get updated as well; may need to resync
-func (s *statusSyncer) Sync(ctx context.Context, snap *v1.ApiSnapshot) error {
+func (s *statusSyncer) Sync(ctx context.Context, snap *v1.StatusSnapshot) error {
 	ctx = contextutils.WithLogger(ctx, "statusSyncer")
 
 	logger := contextutils.LoggerFrom(ctx)
@@ -36,31 +35,88 @@ func (s *statusSyncer) Sync(ctx context.Context, snap *v1.ApiSnapshot) error {
 		len(snap.Ingresses.List()))
 	defer logger.Infof("end sync %v", snap.Hash())
 	logger.Debugf("%v", snap)
+	services := snap.Services.List()
 
-	proxy, err := translateProxy(s.writeNamespace, snap)
+	lbStatus, err := getLbStatus(services)
 	if err != nil {
-		logger.Warnf("snapshot %v was rejected due to invalid config: %v\n"+
-			"ingress proxy will not be updated.", snap.Hash(), err)
 		return err
 	}
 
-	labels := map[string]string{
-		"created_by": "ingress",
-	}
+	for _, ing := range snap.Ingresses.List() {
+		kubeIngress, err := ingress.ToKube(ing)
+		if err != nil {
+			return errors.Wrapf(err, "internal error: converting proto ingress to kube ingress")
+		}
+		kubeIngress.Status.LoadBalancer.Ingress = lbStatus
 
-	var desiredResources gloov1.ProxyList
-	if proxy != nil {
-		logger.Infof("creating proxy %v", proxy.Metadata.Ref())
-		proxy.Metadata.Labels = labels
-		desiredResources = gloov1.ProxyList{proxy}
-	}
+		updatedIngress, err := ingress.FromKube(kubeIngress)
+		if err != nil {
+			return errors.Wrapf(err, "internal error: converting back to proto ingress from kube ingress")
+		}
 
-	if err := s.proxyReconciler.Reconcile(s.writeNamespace, desiredResources, TODO.TransitionFunction, clients.ListOpts{
-		Ctx:      ctx,
-		Selector: labels,
-	}); err != nil {
-		return err
+		if proto.Equal(updatedIngress.KubeIngressStatus, ing.KubeIngressStatus) {
+			continue
+		}
+		if _, err := s.ingressClient.Write(updatedIngress, clients.WriteOpts{Ctx: ctx, OverwriteExisting: true}); err != nil {
+			return errors.Wrapf(err, "writing updated status to kubernetes")
+		}
+		logger.Infof("updated ingress %v with status %v", ing.Metadata.Ref(), lbStatus)
 	}
 
 	return nil
+}
+
+func getLbStatus(services v1.KubeServiceList) ([]kubev1.LoadBalancerIngress, error) {
+	switch len(services) {
+	case 0:
+		return nil, nil
+	case 1:
+		kubeSvc, err := service.ToKube(services[0])
+		if err != nil {
+			return nil, errors.Wrapf(err, "internal error: converting proto svc to kube service")
+		}
+		return ingressStatusFromAddrs(serviceAddrs(kubeSvc)), nil
+	}
+	return nil, errors.Errorf("failed to get lb status: expected 1 ingress service, found %v", func() []core.ResourceRef {
+		var refs []core.ResourceRef
+		for _, svc := range services {
+			refs = append(refs, svc.Metadata.Ref())
+		}
+		return refs
+	}())
+}
+
+func serviceAddrs(svc *kubev1.Service) []string {
+	if svc.Spec.Type == kubev1.ServiceTypeExternalName {
+		return []string{svc.Spec.ExternalName}
+	}
+	var addrs []string
+
+	for _, ip := range svc.Status.LoadBalancer.Ingress {
+		if ip.IP == "" {
+			addrs = append(addrs, ip.Hostname)
+		} else {
+			addrs = append(addrs, ip.IP)
+		}
+	}
+	addrs = append(addrs, svc.Spec.ExternalIPs...)
+
+	return addrs
+}
+
+func ingressStatusFromAddrs(addrs []string) []kubev1.LoadBalancerIngress {
+	var lbi []kubev1.LoadBalancerIngress
+	for _, ep := range addrs {
+		if net.ParseIP(ep) == nil {
+			lbi = append(lbi, kubev1.LoadBalancerIngress{Hostname: ep})
+		} else {
+			lbi = append(lbi, kubev1.LoadBalancerIngress{IP: ep})
+		}
+	}
+
+	sort.SliceStable(lbi, func(a, b int) bool {
+		return lbi[a].IP < lbi[b].IP
+	})
+
+	return lbi
 }

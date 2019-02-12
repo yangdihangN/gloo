64feb121c2d3e0064945583abb06f0c211587d98
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-02-12 13:51:52 -0500

    generate rst

--- generate.go
+++ generate.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	"github.com/solo-io/gloo/pkg/version"
 	"github.com/solo-io/solo-kit/pkg/code-generator/cmd"
+	"github.com/solo-io/solo-kit/pkg/code-generator/docgen/options"
 	"github.com/solo-io/solo-kit/pkg/utils/log"
 )
 
 //go:generate go run generate.go
 
 func main() {
+
 	err := version.CheckVersions()
 	if err != nil {
 		log.Fatalf("generate failed!: %v", err)
 	}
+
 	log.Printf("starting generate")
-	if err := cmd.Run("projects", true, true, nil, nil); err != nil {
+	docs := cmd.DocsOptions{
+		Output: options.Restructured,
+	}
+	if err := cmd.Run("projects", true, &docs, nil, nil); err != nil {
 		log.Fatalf("generate failed!: %v", err)
 	}
 }

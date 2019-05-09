9362b8ec09b56362335796e4ccd7d1cfff7b62e8
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-05-09 15:34:13 -0400

    add tap metadata

--- projects/gloo/pkg/plugins/kubernetes/plugin.go
+++ projects/gloo/pkg/plugins/kubernetes/plugin.go
@@ -5,6 +5,8 @@ import (
 	"net/url"
 
 	envoyapi "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	envoycore "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
+	types "github.com/gogo/protobuf/types"
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/gloo/projects/gloo/pkg/plugins"
 	"github.com/solo-io/gloo/projects/gloo/pkg/xds"
@@ -41,12 +43,31 @@ func (p *plugin) Init(params plugins.InitParams) error {
 
 func (p *plugin) ProcessUpstream(params plugins.Params, in *v1.Upstream, out *envoyapi.Cluster) error {
 	// not ours
-	_, ok := in.UpstreamSpec.UpstreamType.(*v1.UpstreamSpec_Kube)
+	up, ok := in.UpstreamSpec.UpstreamType.(*v1.UpstreamSpec_Kube)
 	if !ok {
 		return nil
+	}
+
+	out.Metadata = &envoycore.Metadata{
+		FilterMetadata: map[string]*types.Struct{
+			"envoy.filters.http.tap": &types.Struct{
+				Fields: map[string]*types.Value{
+					"svc":   tostring(up.Kube.GetServiceName()),
+					"svcns": tostring(up.Kube.GetServiceNamespace()),
+				},
+			},
+		},
 	}
 
 	// configure the cluster to use EDS:ADS and call it a day
 	xds.SetEdsOnCluster(out)
 	return nil
+}
+
+func tostring(s string) *types.Value {
+	return &types.Value{
+		Kind: &types.Value_StringValue{
+			StringValue: s,
+		},
+	}
 }

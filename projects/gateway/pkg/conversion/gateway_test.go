b4dc68fb76b2e5286a018835c859c1c22c7e5beb
Joe Kelley <jsphklly@gmail.com>
2019-07-12 12:09:41 -0400

    upconverter downconverter kickoff

---
+++ projects/gateway/pkg/conversion/gateway_test.go
@@ -0,0 +1,13 @@
+package conversion_test
+
+import . "github.com/onsi/ginkgo"
+
+var _ = Describe("Gateway Converters", func() {
+	Describe("UpConverter", func() {
+
+	})
+
+	Describe("DownConverter", func() {
+
+	})
+})

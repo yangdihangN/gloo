c444d9977cde6ed45ea5e7438e9fde1bc4e8152a
Rick Ducott <rick.ducott@solo.io>
2019-09-13 22:30:59 -0400

    VERSION=$(git describe --tags --dirty) go run solo-builder/pkg/cmd/main.go

--- solo-builder/pkg/config/runtime.go
+++ solo-builder/pkg/config/runtime.go
@@ -5,16 +5,21 @@ import "github.com/kelseyhightower/envconfig"
 type RuntimeConfig struct {
 	Tag     string `envconfig:"TAGGED_VERSION"`
 	Sha     string `envconfig:"SHA"`
-	Version string
+	Version string `envconfig:"VERSION"`
 }
 
 func MustGetRuntimeConfig() *RuntimeConfig {
 	var cfg RuntimeConfig
 	envconfig.MustProcess("", &cfg)
-	if cfg.Tag == "" {
-		cfg.Version = cfg.Sha
-	} else {
-		cfg.Version = cfg.Tag[1:]
+	if cfg.Version == "" {
+		if cfg.Tag == "" {
+			cfg.Version = cfg.Sha
+		} else {
+			cfg.Version = cfg.Tag[1:]
+		}
+	}
+	if cfg.Version == "" {
+		cfg.Version = "undefined"
 	}
 	return &cfg
 }

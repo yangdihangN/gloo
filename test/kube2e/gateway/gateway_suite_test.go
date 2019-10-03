24bd33b5f8a09ccff260f4541920f0c81ff4ae5c
Scott Weiss <sdw35@cornell.edu>
2019-10-03 08:39:10 -0400

    add helpful fail handler

--- test/kube2e/gateway/gateway_suite_test.go
+++ test/kube2e/gateway/gateway_suite_test.go
@@ -8,6 +8,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/solo-io/gloo/pkg/cliutil/install"
+
 	"github.com/gogo/protobuf/types"
 	clienthelpers "github.com/solo-io/gloo/projects/gloo/cli/pkg/helpers"
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
@@ -65,6 +67,14 @@ func StartTestHelper() {
 		return defaults
 	})
 	Expect(err).NotTo(HaveOccurred())
+
+	RegisterFailHandler(func(message string, callerSkip ...int) {
+		glooLogs, _ := install.KubectlOut(nil, "logs", "-n", testHelper.InstallNamespace, "-l", "gloo=gloo")
+		gwLogs, _ := install.KubectlOut(nil, "logs", "-n", testHelper.InstallNamespace, "-l", "gloo=gateway")
+
+		fmt.Fprintf(GinkgoWriter, "\n\n\n\nGLOO LOGS\n\n%s\n\n\n\n", glooLogs)
+		fmt.Fprintf(GinkgoWriter, "\n\n\n\nGATEWAY LOGS\n\n%s\n\n\n\n", gwLogs)
+	})
 
 	skhelpers.RegisterPreFailHandler(helpers.KubeDumpOnFail(GinkgoWriter, "knative-serving", testHelper.InstallNamespace))
 	testHelper.Verbose = true

4be6e7e488c72062cccd165be6fd190d8fcc0488
Graham Goudeau <graham.goudeau@solo.io>
2019-10-11 14:30:02 -0400

    Convert to new usage client

--- projects/gloo/pkg/setup/setup.go
+++ projects/gloo/pkg/setup/setup.go
@@ -2,16 +2,39 @@ package setup
 
 import (
 	"context"
+	"os"
+
+	"github.com/solo-io/gloo/pkg/utils/usage"
+	"github.com/solo-io/gloo/projects/metrics/pkg/metricsservice"
+	"github.com/solo-io/go-utils/contextutils"
+	"github.com/solo-io/reporting-client/pkg/client"
 
 	"github.com/solo-io/gloo/pkg/utils/setuputils"
 	"github.com/solo-io/gloo/projects/gloo/pkg/syncer"
 )
 
 func Main(customCtx context.Context) error {
+	var usageReporter client.UsagePayloadReader
+	metricsStorage, err := metricsservice.NewDefaultConfigMapStorage(os.Getenv("POD_NAMESPACE"))
+	if err != nil {
+		contextutils.LoggerFrom(customCtx).Warnf("Could not create metrics storage loader - will not report usage: %s", err.Error())
+	} else {
+		usageReporter = &usage.DefaultUsageReader{MetricsStorage: metricsStorage}
+	}
+
+	return startSetupLoop(customCtx, usageReporter)
+}
+
+func StartGlooInTest(customCtx context.Context) error {
+	return startSetupLoop(customCtx, nil)
+}
+
+func startSetupLoop(ctx context.Context, usageReporter client.UsagePayloadReader) error {
 	return setuputils.Main(setuputils.SetupOpts{
 		LoggingPrefix: "gloo",
 		SetupFunc:     syncer.NewSetupFunc(),
 		ExitOnError:   true,
-		CustomCtx:     customCtx,
+		CustomCtx:     ctx,
+		UsageReporter: usageReporter,
 	})
 }
